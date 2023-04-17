a79d14a18e2aa612b5331b82f829ed9e01b8f4c9
harkinder-dark <victorvaddely@gmail.com>
2023-04-17 16:36:33 +0000

    first directory in backend

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
-export default function appendToEachArrayValue(array, appendString) {
+export default function appendToEachArrayValue (array, appendString) {
   for (const idx of array) {
     let value = array[idx];
     array[idx] = appendString + value;
   }
-  
+
   return array;
 }
