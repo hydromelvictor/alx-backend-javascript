c259a20824a4a9107d27d00fe8ced49bac614070
harkinder-dark <victorvaddely@gmail.com>
2023-04-17 16:27:31 +0000

    first directory in backend

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -2,17 +2,13 @@ function getCurrentYear() {
   const date = new Date();
   return date.getFullYear();
 }
-  
-export default function getBudgetForCurrentYear(income, gdp, capita) {
-  const budget = {
-    [`income-${getCurrentYear()}`]: income,
-    [`gdp-${getCurrentYear()}`]: gdp,
-    [`capita-${getCurrentYear()}`]: capita,
-  };
 
-  budget[`income-${getCurrentYear()}`] = income;
-  budget[`gdp-${getCurrentYear()}`] = gdp;
-  budget[`capita-${getCurrentYear()}`] = capita;
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  const budget = {};
+
+  budget[Symbol(`income-${getCurrentYear()}`)] = income;
+  budget[Symbol(`gdp-${getCurrentYear()}`)] = gdp;
+  budget[Symbol(`capita-${getCurrentYear()}`)] = capita;
 
   return budget;
 }
