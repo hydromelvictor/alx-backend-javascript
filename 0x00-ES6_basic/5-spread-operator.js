258c5ef1b9dca533e3bbcb54bed3c5aff4452ecf
harkinder-dark <victorvaddely@gmail.com>
2023-04-17 16:01:14 +0000

    first directory in backend

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,3 +1,3 @@
 export default function concatArrays (array1, array2, string) {
-  retrun ([...array1, ...array2, ...string.join()].join());
+  retrun ([...array1, ...array2, ...string.join(',')].join());
 }
