0193f0455b8da7b870b657dcc74f7ef3957e1ffc
harkinder-dark <victorvaddely@gmail.com>
2023-04-17 16:53:14 +0000

    first directory in backend

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,7 +3,7 @@ export default function taskBlock(trueOrFalse) {
     task = true;
     task2 = false;
   }
-  let task = false;
-  let task2 = true;
+  var task = false;
+  var task2 = true;
   return [task, task2];
 }
