9bc64f4718fa50468181539d8cd75af0d7db9c68
harkinder-dark <victorvaddely@gmail.com>
2023-04-17 16:41:51 +0000

    first directory in backend

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,8 +1,9 @@
 export default function createReportObject(employeesList) {
+    
   return {
     allEmployees:{...employeesList},
     getNumberOfDepartments(...employeesList) {
-      return employeesList.length
+      return employeesList.length;
     }
   };
 }
