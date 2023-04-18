078e4df7bc0212fe0e6763badefc46518831b149
harkinder-dark <victorvaddely@gmail.com>
2023-04-18 17:36:28 +0000

    promise async await

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,7 +3,7 @@ import {uploadPhoto, createUser} from './utils.js';
 export default function handleProfileSignup() {
   const upload = uploadPhoto();
   const user = createUser();
-  return Promise.all([upload, user]).then((result) =>  
+  return Promise.all([upload, user]).then((result) =>
     console.log(`${result[0].body} ${result[1].firstName} ${result[1].lastName}`)
   ).catch(() => console.log("Signup system offline"));
 }
