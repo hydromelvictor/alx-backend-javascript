50f44ae545e60cea20e865589c54dfd036e07b18
harkinder-dark <victorvaddely@gmail.com>
2023-04-18 17:27:31 +0000

    promise async await

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,6 +1,6 @@
 export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
-    if (success) resolve({status: 200, body: 'Success'});
-    else reject(console.log("The fake API is not working currently"));
+    if (success) resolve({ status: 200, body: 'Success' });
+    else reject(Error("The fake API is not working currently"));
   });
 }
