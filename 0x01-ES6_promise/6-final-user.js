37a994abb12d5ca4d73417c48e8b7d83f0850c08
harkinder-dark <victorvaddely@gmail.com>
2023-04-19 14:48:39 +0000

    promise 1

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,13 +2,13 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const res = []
+  const res = [];
   try {
     const signup = await signUpUser(firstName, lastName);
     res.push({ status: 'fulfilled', value: signup });
     await uploadPhoto(fileName);
   } catch (err) {
-    res.push({ status: 'rejected', value: err.toString() })
+    res.push({ status: 'rejected', value: err.toString() });
   }
   return res;
 }
