f9fffe7dee02b8af2a55b731d88cf2211588d8e1
harkinder-dark <victorvaddely@gmail.com>
2023-04-19 08:40:00 +0000

    promise 1

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,6 +2,6 @@ export default function handleResponseFromAPI(promise) {
   return promise.then(() => (
     { status: 200, body: 'Success' }
   )).catch(() => Error()).finally(() => {
-    console.log('Got a response from the API'); 
+    console.log("Got a response from the API");
   });
 }
