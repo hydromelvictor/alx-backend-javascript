6e4e5b0878c25c60f9836d7c4ad0c345a2634814
harkinder-dark <victorvaddely@gmail.com>
2023-04-19 08:49:57 +0000

    promise 1

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,9 +1,9 @@
 import { uploadPhoto, createUser } from './utils.js';
 
-export default function handleProfileSignup () {
+export default function handleProfileSignup() {
   const upload = uploadPhoto();
   const user = createUser();
   return Promise.all([upload, user]).then((result) =>
-    console.log(`${result[0].body} ${result[1].firstName} ${result[1].lastName}`)
+    console.log(`${ result[0].body } ${ result[1].firstName } ${ result[1].lastName }`)
   ).catch(() => console.log('Signup system offline'));
 }
