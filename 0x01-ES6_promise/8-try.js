928a0b50bd203a5548cda5f8500adde8309a6797
harkinder-dark <victorvaddely@gmail.com>
2023-04-19 13:44:30 +0000

    promise 1

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,4 +1,4 @@
 export default function divideFunction(numerator, denominator) {
-  if (denominator == 0) throw new Error('cannot divide by 0');
+  if (denominator === 0) throw new Error('cannot divide by 0');
   else return numerator / denominator;
 }
