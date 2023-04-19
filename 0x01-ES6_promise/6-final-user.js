242f3ac2bd1bd9a4c248930ad0e7305f25f0a04f
harkinder-dark <victorvaddely@gmail.com>
2023-04-19 10:33:40 +0000

    promise 1

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,7 +2,7 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  result = []
+  result = [];
   try{
     const signup = await signUpUser(firstName, lastName);
     result.push({ status: 'fulfilled', value: signup});
