3bc220b46b701c6101b9aa815ea6278b68f35ef0
harkinder-dark <victorvaddely@gmail.com>
2023-04-19 08:46:10 +0000

    promise 1

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,4 +1,4 @@
-import {uploadPhoto, createUser} from './utils.js';
+import { uploadPhoto, createUser } from './utils.js';
 
 export default function handleProfileSignup() {
   const upload = uploadPhoto();
