a84b274f811fc3287718804bf8631b0b444e3dc7
harkinder-dark <victorvaddely@gmail.com>
2023-04-19 14:10:27 +0000

    promise 1

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,10 +1,10 @@
 export default async function guardrail(mathFunction) {
-    const res = [];
+  const res = [];
   try {
     const res = mathFunction();
     res.push(res);
   }catch (err) {
-    res.push(err.message);
+    res.push(err.toString());
   }finally {
     res.push('Guardrail was processed');
   }
