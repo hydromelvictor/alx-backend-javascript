5342aad0926cfa614120c0044797e9b6b630fc8c
harkinder-dark <victorvaddely@gmail.com>
2023-04-20 14:25:59 +0000

    add new directory for class learning

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,11 +1,5 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    if (typeof name !== 'string') throw new TypeError('Name must be a string');
-    if (typeof length !== 'number') throw new TypeError('Length must be a number');
-    if (!Array.isArray(students)) throw new TypeError('students must be a array');
-    for (const student of students) {
-      if (typeof student !== 'string') throw new TypeError('students must be a string');
-    }
     this._name = name;
     this._length = length;
     this._students = students;
@@ -28,12 +22,15 @@ export default class HolbertonCourse {
   }
 
   set length(length) {
-    
+    if (typeof length !== 'number') throw new TypeError('Length must be a number');
     this._length = length;
   }
 
   set students(students) {
-    
+    if (!Array.isArray(students)) throw new TypeError('students must be a array');
+    for (const student of students) {
+      if (typeof student !== 'string') throw new TypeError('students must be a string');
+    }
     this._students = students;
   }
 }
