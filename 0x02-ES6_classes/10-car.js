ec9b5b14a049faa049e825884a2bbf9ac04bfe4c
harkinder-dark <victorvaddely@gmail.com>
2023-04-20 15:09:05 +0000

    rewiew code

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,6 +1,6 @@
 export default class Car {
   constructor(brand, motor, color) {
-    if (typeof brand !== 'string' && typeof motor !== 'string' && typeof color !== 'string') throw new Error();
+    if (typeof brand !== 'string' || typeof motor !== 'string' || typeof color !== 'string') throw new Error();
     this._brand = brand;
     this._motor = motor;
     this._color = color;
