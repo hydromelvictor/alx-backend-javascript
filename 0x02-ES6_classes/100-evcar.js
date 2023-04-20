a7d6e76986347a76f8efb3e5f1dadb95b4e31c21
harkinder-dark <victorvaddely@gmail.com>
2023-04-20 14:57:16 +0000

    add new directory for class learning

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -2,6 +2,7 @@ import Car from './10-car.js';
 
 export default class EVCar extends Car {
   constructor(brand, motor, color, range){
+    if (typeof brand !== 'string' || typeof motor !== 'string' || typeof color !== 'string' || typeof range !== 'string') throw new Error();
     super(brand, motor, color);
     this._range = range;
   }
