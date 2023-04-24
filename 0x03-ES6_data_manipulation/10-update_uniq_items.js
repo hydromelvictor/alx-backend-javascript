6c626a0a3f6c4397077e3041fca51bce770255f2
harkinder-dark <victorvaddely@gmail.com>
2023-04-24 19:20:44 +0000

    data manipulations

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,8 +1,8 @@
 export default function updateUniqueItems(mapp) {
   if (!(mapp instanceof Map)) throw new Error('Cannot process');
-  for (const val of mapp.values()) {
+  for (const [key, val] of mapp.entries) {
     if (val === 1) {
-      val = 100;
+      mapp.set(key, 100);
     }
   }
   return mapp;
