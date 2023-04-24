2b5cf067d26b91f0ca1b6471cc825bd7a2826881
harkinder-dark <victorvaddely@gmail.com>
2023-04-24 19:30:31 +0000

    data manipulations

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,6 @@
 export default function cleanSet(set, startString) {
   let res = [];
+  if (!startString || !startString.length) return res;
   for (const elt of set) {
     if (elt && elt.startsWith(startString)) {
       res.push(elt.slice(startString.length));
