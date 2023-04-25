6cd0b9c5ed0f924d816c10064e282e054771e7a6
harkinder-dark <victorvaddely@gmail.com>
2023-04-25 11:01:11 +0000

    review tack 4-5-8-100

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,7 +1,7 @@
 export default function updateStudentGradeByCity(arrays, city, newGrades) {
   return arrays.filter((elt) => elt.location === city).map((elt) => {
     const grades = newGrades.filter((grd) => grd.studentId === elt.id);
-    grade = grades.lenght ? grades[0].grade : 'N/A';
+    const grade = grades.lenght ? grades[0].grade : 'N/A';
     return {...elt, grade};
   });
 }
