50f275891b1bba6359a4f9a3a3e0d35e705f3712
harkinder-dark <victorvaddely@gmail.com>
2023-06-13 17:48:33 +0000

    update

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,11 @@
-const process = require('process');
-
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
   if (name !== null) {
-    process.stdout.write(`Your name is: ${name}\n`);
+    process.stdout.write(`Your name is: ${name}`);
   }
 });
 
-process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
+process.stdin.on('close', () => {
+  process.stdout.write('This important software is now closing');
 });
