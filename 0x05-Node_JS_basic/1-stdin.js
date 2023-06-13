4a9a18d61d3915ce910ed21ac61487c969e4ef24
harkinder-dark <victorvaddely@gmail.com>
2023-06-13 17:50:22 +0000

    update

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -7,5 +7,5 @@ process.stdin.on('readable', () => {
 });
 
 process.stdin.on('close', () => {
-  process.stdout.write('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 });
