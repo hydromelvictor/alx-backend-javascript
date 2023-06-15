164f219568c318365319b845eb03cd4871c9f6ae
harkinder-dark <victorvaddely@gmail.com>
2023-06-15 18:25:56 +0000

    add new file

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -4,8 +4,8 @@ const getApi = require('./6-payment_token');
 const chai  = require('chai');
 const expect = chai.expect;
 
-describe('async test', (done) => {
-  it('', () => {
+describe('async test', () => {
+  it('', (done) => {
     expect(getApi(true)).to.equal({data: 'Successful response from the API' });
     done();
   });
