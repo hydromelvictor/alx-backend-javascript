a17b6b226d38dc1ea9841b24d585dce1945251c9
harkinder-dark <victorvaddely@gmail.com>
2023-06-15 13:34:39 +0000

    add 1-calcul

---
+++ 0x06-unittests_in_js/1-calcul.js
@@ -0,0 +1,19 @@
+function calculateNumber(type, a, b) {
+  const rn_a = Math.round(a);
+  const rn_b = Math.round(b);
+  if (type === 'SUM') {
+    return rn_a + rn_b;
+  }
+  if (type === 'SUBTRACT') {
+    return rn_a - rn_b;
+  }
+  if (type === 'DIVIDE') {
+    if (rn_b === 0) {
+        return 'Error';
+    } else {
+        return rn_a / rn_b;
+    }
+  }
+}
+
+module.exports = calculateNumber;
---
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,21 @@
+const assert = require('assert');
+const { it, describe } = require('mocha');
+const calculateNumber = require('./1-calcul');
+
+describe("calculateNumber function test to 1-calcul", function() {
+    it("correct clacul -1", function() {
+        assert.equal(calculateNumber('SUM', 0.4, 0.1), 0);
+    });
+
+    it("correct calcul -2", function() {
+        assert.equal(calculateNumber('SUBTRACT', 5.8, 3.1), 3);
+    });
+
+    it("correct calcul -3", function() {
+        assert.equal(calculateNumber('DIVIDE', 4, 1.8), 2);
+    });
+
+    it("divide error", function() {
+        assert.equal(calculateNumber('DIVIDE', 12, 0.4), 'Error');
+    });
+});
