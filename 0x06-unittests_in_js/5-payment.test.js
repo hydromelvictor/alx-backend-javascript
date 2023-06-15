fd9410682c6c805fadecb028c648f69277d56c7f
harkinder-dark <victorvaddely@gmail.com>
2023-06-15 17:56:23 +0000

    add new file

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -8,27 +8,22 @@ const sendPaymentRequestToApi = require('./5-payment');
 
 describe('test', () => {
     let print;
-  test('test -1', () => {
-    beforeEach(() => {
-        print = sinon.spy(console, 'log');
-    });
+
+  beforeEach(() => {
+    print = sinon.spy(console, 'log');
+  });
+  it('test -1', () => {
     sendPaymentRequestToApi(100, 20);
     expect(print.calledOnce).to.be.true;
     expect(console.log('The total is: 120')).to.be.all;
-    afterEach(() => {
-        print.restore();
-    });
   });
 
-  test('test -2', () => {
-    beforeEach(() => {
-        print = sinon.spy(console, 'log');
-    });
+  it('test -2', () => {
     sendPaymentRequestToApi(10, 10);
     expect(print.calledOnce).to.be.true;
     expect(console.log('The total is: 20')).to.be.all;
-    afterEach(() => {
-        print.restore();
-    });
+  });
+  afterEach(() => {
+    print.restore();
   });
 });
