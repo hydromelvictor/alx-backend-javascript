c5b17b4480c251e3a2c9210c7d8f371ec24e2156
harkinder-dark <victorvaddely@gmail.com>
2023-06-15 18:23:53 +0000

    add new file

---
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -0,0 +1,7 @@
+function getPaymentTokenFromAPI(success) {
+  return new Promise((resolve) => {
+    if (success) resolve({data: 'Successful response from the API' });
+  });
+}
+
+module.exports = getPaymentTokenFromAPI;
---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,12 @@
+const { it, describe } = require('mocha');
+const getApi = require('./6-payment_token');
+
+const chai  = require('chai');
+const expect = chai.expect;
+
+describe('async test', (done) => {
+  it('', () => {
+    expect(getApi(true)).to.equal({data: 'Successful response from the API' });
+    done();
+  });
+});
