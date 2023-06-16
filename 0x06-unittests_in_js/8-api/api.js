8c3cc572971a4704562faa1c94030e2f65ffe26e
harkinder-dark <victorvaddely@gmail.com>
2023-06-16 13:15:39 +0000

    update

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -4,15 +4,12 @@ const app = express();
 const port = 7865;
 const hostname = 'localhost';
 
-app.get('/', (req, res, next) => {
-  console.log('API available on localhost port 7865');
-  next();
-}, (req, res) => {
+app.get('/', (req, res) => {
     res.send('Welcome to the payment system');
 });
 
 app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`API available on localhost port ${port}`);
 });
 
 module.exports = app;
--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,14 +1,16 @@
 const app = require('./api');
 const { expect, request } = require('chai');
 const { it, describe } = require('mocha');
+const request = require('request');
+const url = 'http://localost:7865/'
 
 describe('test request', () => {
-  it('test -0', () => {
-    request(app)
-      .get('/index')
-      .end((err, res) => {
-        expect(res.status).to.equal(200);
-        expect(res.body).to.equal('Welcome to the payment system');
+  it('test -0', (done) => {
+    request(url)
+      .get(url, (error, response, body) => {
+        expect(response.statusCode).to.be.equal(200);
+        expect(body).to.be.equal('Welcome to the payment system');
+        done();
       });
   });
 });
