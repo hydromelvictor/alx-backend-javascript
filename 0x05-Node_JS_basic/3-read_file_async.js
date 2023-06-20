99b995235af83db048b3b26a83a306344e9295ed
harkinder-dark <victorvaddely@gmail.com>
2023-06-20 07:53:06 -0700

    update

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -5,31 +5,31 @@ function countStudents(path) {
   return new Promise((resolve) => {
     fs.readFile(path, 'utf-8', (err, data) => {
       if (err) {
-        throw Error('Cannot load the database');
+        throw new Error('Cannot load the database');
+       } else {
+        const fields = new Set();
+        const students = [];
+        data = data.trim().split('\n');
+        const newlines = [];
+        for (let i = 1; i < data.length; i++) {
+          newlines.push(data[i]);
+        }
+        newlines.forEach(elt => {
+          let line = elt.split(',');
+          if (line.length === 4) { 
+            fields.add(line[3]);
+            students.push(line);
+	  }
+        });
+        console.log(`Number of students: ${students.length}`);
+        fields.forEach(elt => {
+  	  const studie = students.filter(ys => ys[3] === elt);
+	  const names = studie.map(ws => ws[0]);
+          console.log(`Number of students in ${elt}: ${names.length}. List: ${names.join(',')}`);
+        });
       }
-      const fields = new Set();
-      const students = [];
-      data = data.trim().split('\n');
-      const newlines = [];
-      for (let i = 1; i < data.length; i++) {
-        newlines.push(data[i]);
-      }
-      newlines.forEach(elt => {
-        let line = elt.split(',');
-        if (line.length === 4) { 
-          fields.add(line[3]);
-          students.push(line);
-	}
-      });
-      console.log(`Number of students: ${students.length}`);
-      fields.forEach(elt => {
-	const studie = students.filter(ys => ys[3] === elt);
-	const names = studie.map(ws => ws[0]);
-        console.log(`Number of students in ${elt}: ${names.length}. List: ${names.join(',')}`);
-      });
-      resolve(students)
     });
   });
-};
+}
 
 module.exports = countStudents;
