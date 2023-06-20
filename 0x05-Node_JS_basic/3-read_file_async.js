aa24752ddd14ae17e0a225221067f6d0f4e7f349
harkinder-dark <victorvaddely@gmail.com>
2023-06-20 07:31:01 -0700

    update

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -2,38 +2,34 @@ const fs = require('fs');
 
 const { resolve } = require('path');
 function countStudents(path) {
-  if (fs.existsSync(path)) {
-    return new Promise((resolve) => {
-      fs.readFile(path, 'utf-8', (err, data) => {
-        if (err) {
-          throw Error('Cannot load the database');
-        }
-        const fields = new Set();
-        const students = [];
-	data = data.trim().split('\n');
-	const newlines = [];
-	for (let i = 1; i < data.length; i++) {
-          newlines.push(data[i]);
+  return new Promise((resolve) => {
+    fs.readFile(path, 'utf-8', (err, data) => {
+      if (err) {
+        throw Error('Cannot load the database');
+      }
+      const fields = new Set();
+      const students = [];
+      data = data.trim().split('\n');
+      const newlines = [];
+      for (let i = 1; i < data.length; i++) {
+        newlines.push(data[i]);
+      }
+      newlines.forEach(elt => {
+        let line = elt.split(',');
+        if (line.length === 4) { 
+          fields.add(line[3]);
+          students.push(line);
 	}
-        newlines.forEach(elt => {
-          let line = elt.split(',');
-          if (line.length === 4) { 
-            fields.add(line[3]);
-            students.push(line);
-	  }
-        });
-        console.log(`Number of students: ${students.length}`);
-        fields.forEach(elt => {
-	  const studie = students.filter(ys => ys[3] === elt);
-	  const names = studie.map(ws => ws[0]);
-          console.log(`Number of students in ${elt}: ${names.length}. List: ${names.join(',')}`);
-        });
-        resolve(students)
       });
+      console.log(`Number of students: ${students.length}`);
+      fields.forEach(elt => {
+	const studie = students.filter(ys => ys[3] === elt);
+	const names = studie.map(ws => ws[0]);
+        console.log(`Number of students in ${elt}: ${names.length}. List: ${names.join(',')}`);
+      });
+      resolve(students)
     });
-  } else {
-    throw Error('Cannot load the database');
-  }
+  });
 };
 
 module.exports = countStudents;
