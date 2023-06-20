0660ed8c72a64aef3fd650cc5c40a69a0407eabe
harkinder-dark <victorvaddely@gmail.com>
2023-06-20 05:08:19 -0700

    update

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -5,24 +5,23 @@ function countStudents(path) {
     const data = fs.readFileSync(path, 'utf-8');
     let students = [];
     const fields = new Set();
-    data.split('\n').forEach(element => {
+    data = data.split('\n').shift()
+    data.forEach(element => {
       const line = element.split(',');
-      fields.add(line[3]);
-      students.push(line);
+      if (line.length == 4) {
+     	 fields.add(line[3]);
+     	 students.push(line);
+      }
     });
-    students = students.shift();
     console.log(`Number of students: ${students.length}`);
     const firstname = [];
     fields.forEach(elt => {
-      students.forEach(ps => {
-        if (ps[3] === elt) {
-            firstname.push(ps[0]);
-        }
-      });
-      console.log(`Number of students in ${elt}: ${firstname.length}. List: ${firstname.join(',')}`);
+      const studie = students.filter(ps => ps[3] === elt);
+      const names = studies.map(ps => ps[0]);
+      console.log(`Number of students in ${elt}: ${names.length}. List: ${names.join(',')}`);
     });
   } catch (e) {
-    throw Error('Cannot load the database');
+    throw Error("Cannot load the database");
   }
 }
 
